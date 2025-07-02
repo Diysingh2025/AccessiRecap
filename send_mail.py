e5833446966166a8c141148881e53b4089fb7c47
DIYA SINGH <diya.singh@lilly.com>
2025-07-02 15:54:36 -0400

    adding sending summary audio in send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -1,7 +1,7 @@
 import smtplib
 from email.message import EmailMessage
 
-def send_gmail_email( app_password, recipients, body, attachment_path):
+def send_gmail_email( app_password, recipients, body, attachment_path, audio_path=None):
     sender_email = "diyasingh1015@gmail.com"
     msg = EmailMessage()
     msg['Subject'] = "Meeting Summary PDF"
@@ -13,6 +13,11 @@ def send_gmail_email( app_password, recipients, body, attachment_path):
         file_data = f.read()
         from os.path import basename
         msg.add_attachment(file_data, maintype="application", subtype="pdf", filename=basename(attachment_path))
+
+    if audio_path:
+        with open(audio_path, "rb") as f:
+            audio_data = f.read()
+            msg.add_attachment(audio_data, maintype="audio", subtype="mpeg", filename=basename(audio_path))
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
         smtp.login(sender_email, app_password)
