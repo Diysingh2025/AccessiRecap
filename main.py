a5c67b5970bf1b0b607868c3fd25a1c757f0537b
DIYA SINGH <diya.singh@lilly.com>
2025-07-02 15:55:41 -0400

    Minor change in main.py

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ if __name__ == "__main__":
     summary, font_choice, font_size, font_color, filename = get_user_input()
     save_to_pdf(summary, font_choice, font_size, font_color, filename)
 
+    audio_path = None
+    audio_choice = input("Do you want to generate an audio summary? (yes/no): ").strip().lower()
+    if audio_choice in ("y", "yes"):
+        audio_path = text_to_audio(summary)
+
     send_choice = input("Want to send this PDF to Stakeholders? (yes/no): ").strip().lower()
     if send_choice in ("y", "yes"):
         password = pwinput.pwinput(prompt="Your app password: ", mask="*")
         recipients = input("Recipient mails (separated by comma): ").strip().split(",")
         body = "Attached is the summary PDF for your reference."
-        send_gmail_email(password, recipients, body, filename)
-        
-    audio_choice = input("Do you want to generate an audio summary? (yes/no): ").strip().lower()
-    if audio_choice in ("y", "yes"):
-        text_to_audio(summary)
+        send_gmail_email(password, recipients, body, filename, audio_path)
