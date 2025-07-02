b32df966a037aa088369b47a38724404b5c4ca08
DIYA SINGH <diya.singh@lilly.com>
2025-07-02 15:52:30 -0400

    Updated text_to_audio.py

--- text_to_audio.py
+++ text_to_audio.py
@@ -7,5 +7,6 @@ def text_to_audio(summary_text, output_file="summary_audio.mp3"):
         tts.save(output_file)
         print(f"\n Audio summary saved as: {output_file}")
         os.system(f"start {output_file}") 
+        return output_file
     except Exception as e:
         print(f" Error generating audio: {e}")
